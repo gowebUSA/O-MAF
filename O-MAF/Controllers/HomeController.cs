b7aa51aaeb9102f0240d3ae5c9208651bb042bf0
Richard Go <60155239+gowebUSA@users.noreply.github.com>
2020-03-14 09:58:15 -0700

    Adds more views, controllers, and models.

--- O-MAF/Controllers/HomeController.cs
+++ O-MAF/Controllers/HomeController.cs
@@ -20,18 +20,65 @@ namespace O_MAF.Controllers
 
         public IActionResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning!" : "Good Afternoon!";
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
+                        
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+        [HttpGet]
+        public ViewResult LoginForm()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+        }
+        public ViewResult InitiateForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult LoginForm(LoginResponse loginResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                LoginRepository.AddResponse(loginResponse);
+                return View("WhoseLogin", loginResponse);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+        }
+        public ViewResult InitiateForm(InitiateResponse initiateResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                InitiateRepository.AddResponse(initiateResponse);
+                return View("Workload", initiateResponse);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+        }
+        public ViewResult LoginResponse()
+        {
+            return View(LoginRepository.Responses);          //LoginRepository.Responses.Where(r => r.WillAttend == true)
+        }
+        public ViewResult InitiateResponse()
+        {
+            return View(InitiateRepository.Responses.Where(r => r.AcftUp == true));
         }
     }
 }
---
+++ O-MAF/Models/InitiateRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace O_MAF.Models
+{
+    public class InitiateRepository
+    {
+        private static List<InitiateResponse> responses = new List<InitiateResponse>();
+        public static IEnumerable<InitiateResponse> Responses
+        {
+            get
+            { return responses; }
+        }
+        public static void AddResponse(InitiateResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
