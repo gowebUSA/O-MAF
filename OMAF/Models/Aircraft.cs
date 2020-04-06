109c65cbafdcb0c57f7493580c80abf962603bc0
Richard Go <60155239+gowebUSA@users.noreply.github.com>
2020-04-06 16:17:02 -0700

    Updates Privacy policy, DoD Consent, and labels.

--- OMAF/Models/Aircraft.cs
+++ OMAF/Models/Aircraft.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OMAF.Models
 {
@@ -10,8 +11,11 @@ namespace OMAF.Models
     {
         public int Id { get; set; }
         public string Buno { get; set; }    //Genre
+        //
+        [Display(Name = "System Reason")]
         public string SysReason { get; set; }   //Title
-
+        //
+        [Display(Name = "Date")]
         //[DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public int UDP { get; set; }        //Price
